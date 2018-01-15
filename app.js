337dd2d8aada214e04e6f1d63c325c41a395d927
Jenny Knuth <inkboco@gmail.com>
2018-01-15 07:18:57 -0700

    hardcoded colors, need import

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// var colors = [
-//   {
-//     "name": "INDIANRED",
-//     "hex": "#CD5C5C",
-//     "rgb": "RGB(205, 92, 92)",
-//     "families": ["red", "brown"]
-//   },
-//   {
-//     "name": "LIGHTCORAL",
-//     "hex": "#F08080",
-//     "rgb": "RGB(240, 128, 128)",
-//     "families": ["red", "pink", "coral", "light"]
-//   },
-//   {
-//     "name": "SALMON",
-//     "hex": "#FA8072",
-//     "rgb": "RGB(250, 128, 114)",
-//     "families": ["red", "pink", "orange", "salmon"]
-//   }
-// ];
+var colors = [
+  {
+    "name": "INDIANRED",
+    "hex": "#CD5C5C",
+    "rgb": "RGB(205, 92, 92)",
+    "families": ["red", "brown"]
+  },
+  {
+    "name": "LIGHTCORAL",
+    "hex": "#F08080",
+    "rgb": "RGB(240, 128, 128)",
+    "families": ["red", "pink", "coral", "light"]
+  },
+  {
+    "name": "SALMON",
+    "hex": "#FA8072",
+    "rgb": "RGB(250, 128, 114)",
+    "families": ["red", "pink", "orange", "salmon"]
+  }
+];
 
-var colors = require('colors.js');
+// var colors = require('colors.js');
 var section = document.getElementsByTagName('section')
 
 var colorblocks = [];
